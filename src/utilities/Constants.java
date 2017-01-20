433fc7a6a5f56bf577e48b5ffd47fde158625f07
franco7scala <franco7scala@gmail.com>
2017-01-20 11:08:17 +0100

    edited constatnts

--- src/utilities/Constants.java
+++ src/utilities/Constants.java
@@ -8,6 +8,6 @@ public interface Constants {
 	
 	public static final String DECOMPRESSION_EXTENSION = "decompressing";
 	public static final String ARITHMETIC_COMPRESSION_EXTENSION = "ac";
-	public static final int MAX_READABLE_BYTES = 8;
+	public static final int MAX_READABLE_BYTES = 16;
 	
 }
