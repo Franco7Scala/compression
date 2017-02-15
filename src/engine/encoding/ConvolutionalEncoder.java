410c27b55c8712b493430513ab630c79afdd8e8a
franco7scala <franco7scala@gmail.com>
2017-02-15 12:58:28 +0100

    convolutional encoder BUG FIX

--- src/engine/encoding/ConvolutionalEncoder.java
+++ src/engine/encoding/ConvolutionalEncoder.java
@@ -33,6 +33,7 @@ public class ConvolutionalEncoder implements Encoder {
 		}
 		Fragmenter fragmenter = new Fragmenter(fileName, parameters.blockSize);
 		System.out.println("fZ: " + fragmenter.getFileSize());
+		System.out.println("BZ: " + parameters.blockSize);
 		parameters.fileSize = (int)fragmenter.getFileSize();
 		if ( (fragmenter.getFileSize()%8) == 0 ) {
 			parameters.blockQuantity = (int) fragmenter.getFileSize()/8;
@@ -48,53 +49,80 @@ public class ConvolutionalEncoder implements Encoder {
 				delegate.notifyAdvancementEncoding((float)fragmenter.getCurrentFragment()/(float)fragmenter.getFileSize());
 			}
 			BitSet input = BitSet.valueOf(fragmenter.nextFragment());
-			System.out.println("INPUT");
-			for ( int ppp = 0; ppp < (8 * parameters.k) ; ppp ++) {
-				System.out.print( (input.get(ppp)? 1 : 0) +"");
-			}
-			System.out.print("\n");
-			//System.out.println("read: " + input);
-			parameters.coder.startEncoding();
-			BitSet output = new BitSet( 8 * parameters.n );
-			System.out.println("...k: " + parameters.k);
-			int offset = 0;
-			for ( int i = 0; i < input.size(); /*i += parameters.k*/ ) {
-				//System.out.println("-----i= " + i);
-				BitSet toEncode = new BitSet();
-				//System.out.println("LLL");
-				for ( int f = 0; f < parameters.k; f ++, i ++ ) {
-					//System.out.print( (pene.get(ppp)? 1 : 0) +"");
-					toEncode.set(f, input.get(i));
+			int inputIndex = 0;
+			for ( ; inputIndex < parameters.blockSize; inputIndex ++ ) {
+				
+				
+				
+				
+				
+				
+				System.out.println("INPUT");
+				for ( int ppp = inputIndex; ppp < (8 * parameters.k) ; ppp ++) {
+					System.out.print( (input.get(ppp)? 1 : 0) +"");
 				}
-				//System.out.print("\n");
-				//System.out.println("preso: " + (toEncode.get(0)? 1 : 0));
-				BitSet tempFragment = parameters.coder.getNextSymbolEncoded(toEncode);
-				//System.out.print("do:\n");
-				for ( int j = 0; j < parameters.n; j ++ , offset ++ ) {
-					output.set(offset, tempFragment.get(j));
-					//System.out.print( (tempFragment.get(j)? 1 : 0) +"");
+				System.out.print("\n");
+				//System.out.println("read: " + input);
+				parameters.coder.startEncoding();
+				BitSet output = new BitSet( 8 * parameters.n );
+				System.out.println("...k: " + parameters.k);
+				int offset = 0;
+				for ( int i = 0; i < input.size(); /*i += parameters.k*/ ) {
+					//System.out.println("-----i= " + i);
+					BitSet toEncode = new BitSet();
+					//System.out.println("LLL");
+					for ( int f = 0; f < parameters.k; f ++, i ++ ) {
+						//System.out.print( (pene.get(ppp)? 1 : 0) +"");
+						toEncode.set(f, input.get(i + (inputIndex * parameters.blockSize)));
+					}
+					//System.out.print("\n");
+					//System.out.println("preso: " + (toEncode.get(0)? 1 : 0));
+					BitSet tempFragment = parameters.coder.getNextSymbolEncoded(toEncode);
+					//System.out.print("do:\n");
+					for ( int j = 0; j < parameters.n; j ++ , offset ++ ) {
+						output.set(offset, tempFragment.get(j));
+						//System.out.print( (tempFragment.get(j)? 1 : 0) +"");
+					}
+				//	System.out.print("\n");
 				}
-			//	System.out.print("\n");
-			}
-			System.out.print("OUT\n");
-			for ( int ppp = 0; ppp < (8 * parameters.n) ; ppp ++) {
-				System.out.print( (output.get(ppp)? 1 : 0) +"");
-			}
-			System.out.print("\n");
-			for ( int k = 0; k < (8 * parameters.n); ) {
-				byte toPrint = 0;
-				for ( int x = 0; x < 8; x ++, k ++ ) {
-					toPrint = Support.addBitToPosition(toPrint, x, (output.get(k)? 1 : 0));
+				System.out.print("OUT\n");
+				for ( int ppp = 0; ppp < (8 * parameters.n) ; ppp ++) {
+					System.out.print( (output.get(ppp)? 1 : 0) +"");
 				}
-				sequenceEncoded[indexSequence] = toPrint;
-				indexSequence ++;
+				System.out.print("\n");
+				for ( int k = 0; k < (8 * parameters.n); ) {
+					byte toPrint = 0;
+					for ( int x = 0; x < 8; x ++, k ++ ) {
+						toPrint = Support.addBitToPosition(toPrint, x, (output.get(k)? 1 : 0));
+					}
+					sequenceEncoded[indexSequence] = toPrint; //TODO fix NON multipli di 8
+					System.out.println("--------WRITING IN: " + indexSequence);
+					indexSequence ++;
+				}
+				/*for (byte b : output.toByteArray()) {
+					sequenceEncoded[indexSequence] = b;
+					//System.out.println("ENC: " + sequenceEncoded[indexSequence]);
+					indexSequence ++;
+				}*/
+				parameters.coder.stopEncoding();
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
 			}
-			/*for (byte b : output.toByteArray()) {
-				sequenceEncoded[indexSequence] = b;
-				//System.out.println("ENC: " + sequenceEncoded[indexSequence]);
-				indexSequence ++;
-			}*/
-			parameters.coder.stopEncoding();
+			
+			
+			
+			
+			
 		}
 		return sequenceEncoded;
 	}
@@ -103,13 +131,13 @@ public class ConvolutionalEncoder implements Encoder {
 	public boolean decode(byte[] input, EncoderParameters parameters) {
 		FileOutputStream outputStream = null;
 		try {
-			//PrintWriter writer = new PrintWriter(parameters.decodingOut);
-			//writer.print("");
-			//writer.close();
-			//outputStream = new FileOutputStream(parameters.decodingOut, true);
+			PrintWriter writer = new PrintWriter(parameters.decodingOut);
+			writer.print("");
+			writer.close();
+			outputStream = new FileOutputStream(parameters.decodingOut, true);
 			System.out.println("--------INPUT-------");
 			BitSet inputBitSet = BitSet.valueOf(input);
-			for ( int ppp = 0; ppp < (8 * parameters.n)*4 ; ppp ++) {
+			for ( int ppp = 0; ppp < (((8 * parameters.n) * parameters.blockQuantity) * parameters.blockSize); ppp ++) {
 				System.out.print( (inputBitSet.get(ppp)? 1 : 0) +"");
 			}
 			System.out.print("\n");
@@ -118,7 +146,7 @@ public class ConvolutionalEncoder implements Encoder {
 			System.out.println("bSuze: " + parameters.blockSize);
 			System.out.println("bQQQ: " + parameters.blockQuantity);
 			System.out.println("n: " + parameters.n);
-			for ( int i = 0; i < ((8 * parameters.n) * parameters.blockQuantity); ) {
+			for ( int i = 0; i < (((8 * parameters.n) * parameters.blockQuantity) * parameters.blockSize); ) {
 				//System.out.println("i_" + i);
 				if ( delegate != null ) {
 					delegate.notifyAdvancementEncoding((float)i/(float)inputBitSet.size());
@@ -128,23 +156,52 @@ public class ConvolutionalEncoder implements Encoder {
 					parameters.coder.stopDecoding();
 					parameters.coder.startDecoding();
 				}
-
-				BitSet toDecode = new BitSet();
-				//System.out.println("+++LLL    " + parameters.n + " i=" + i);
-				for ( int f = 0; f < parameters.n; f ++, i ++ ) {
-					
-					toDecode.set(f, inputBitSet.get(i));
-					//System.out.print( (toDecode.get(f)? 1 : 0) +"");
-				}
-				//System.out.println("         mmm");
 				
-				BitSet fragment = parameters.coder.getNextSymbolDecoded(toDecode);
+				
+				int indexReached = 0;
+				byte toPrint = 0;
+				while ( indexReached != 8 ) {
+					
+					BitSet toDecode = new BitSet();
+					//System.out.println("+++LLL    " + parameters.n + " i=" + i);
+					for ( int f = 0; f < parameters.n; f ++, i ++ ) {
+						
+						toDecode.set(f, inputBitSet.get(i));
+						//System.out.print( (toDecode.get(f)? 1 : 0) +"");
+					}
+					//System.out.println("         mmm");
+					
+					BitSet fragment = parameters.coder.getNextSymbolDecoded(toDecode);
+					
+					for ( int x = 0; x < parameters.k; x ++, indexReached ++ ) {
+						toPrint = Support.addBitToPosition(toPrint, indexReached, (fragment.get(x)? 1 : 0));
+					}
+					
+				}
+				
+				
+				
+				
+				
+				
+				
+				
 				//System.out.println("F: " + fragment.toString());
 				//outputStream.write(fragment.toByteArray());
-				//System.out.print("OUT\n");
-				for ( int ppp = 0; ppp < parameters.k ; ppp ++) {
-					System.out.print( (fragment.get(ppp)? 1 : 0) +"");
+				/*
+				System.out.print("\n");
+				
+				for ( int x = 0; x < 8; x ++) {
+					toPrint = Support.addBitToPosition(toPrint, x, (fragment.get(x)? 1 : 0));
+					System.out.print("" + Support.getBitValue(toPrint, x));
 				}
+				System.out.print("\n");*/
+				outputStream.write(toPrint);
+				
+				//System.out.print("OUT\n");
+				/*for ( int ppp = 0; ppp < parameters.k ; ppp ++) {
+					System.out.print( (fragment.get(ppp)? 1 : 0) +"");
+				}*/
 				//System.out.print("\n");
 			}
 			parameters.coder.stopDecoding();
@@ -155,9 +212,9 @@ public class ConvolutionalEncoder implements Encoder {
 		}
 		finally {
 			try {
-				//outputStream.close();
+				outputStream.close();
 				time = System.currentTimeMillis() - time;
-				throw new IOException();//TODO cacciare
+				//throw new IOException();//TODO cacciare
 			} catch (IOException e) {}
 		}
 	}
